c6ec5c24829e6cc49b01db81d182beb2d97a6baf
Matthew Messinger <mattmess1221@gmail.com>
2020-06-13 23:59:57 -0400

    Use explicit dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,8 +27,8 @@ repositories {
 
 dependencies {
     implementation("net.fabricmc:fabric-loom:0.2.7-SNAPSHOT")
-    implementation("net.fabricmc:tiny-remapper:0.2.2.+")
-    implementation("net.fabricmc:tiny-mappings-parser:0.3.0+")
+    implementation("net.fabricmc:tiny-remapper:0.3.1.71")
+    implementation("net.fabricmc:tiny-mappings-parser:0.3.0+build.17")
     implementation("com.google.code.gson:gson:2.8.5")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 }
